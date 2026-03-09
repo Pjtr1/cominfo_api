b1080e43ea8c1f32dd22d54d2ba3a769e9073b52
Pjtr <pijitr1234@gmail.com>
2026-03-10 05:03:01 +0700

    adjust promt

--- services/llm_answer.py
+++ services/llm_answer.py
@@ -3,13 +3,14 @@ from .prompts import FIELD_CONTEXT_PROMPT
 def call_llm_answer(user_message: str, context: str) -> str:
     prompt = f"""
 You are a chat ai assistant in a university restaurants app. you have access to the informations of the canteens and all the restaurants in each of them from the app's database
+Answer only whats asked
 User message:
 {user_message}
 
 Database information fetched for this reponse:
 {context}
 
-Respond naturally to the user.
+Respond naturally to the user in a sentences.
 """
     return call_llm(prompt)
 
