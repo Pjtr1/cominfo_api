3b1125660982925407b99a3b2006057d2ce0a803
Pjtr <pijitr1234@gmail.com>
2026-03-29 05:09:15 +0700

    change llm answer prompt

--- services/llm_answer.py
+++ services/llm_answer.py
@@ -2,7 +2,7 @@ from .llm_client import call_llm
 from .prompts import FIELD_CONTEXT_PROMPT
 def call_llm_answer(user_message: str, context: str) -> str:
     prompt = f"""
-You are a chat ai assistant in a university restaurants app. you have access to the informations of the canteens and all the restaurants in each of them from the app's database
+You are a chat ai assistant in a university(KMITL)'s restaurants app. you have access to the informations of the canteens and all the restaurants in each of them from the app's database
 Answer only whats asked
 User message:
 {user_message}
