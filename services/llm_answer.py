a030dc3083735483e75aa69bf1723a6d7a98615a
Pjtr <pijitr1234@gmail.com>
2026-03-29 05:15:14 +0700

    more prompt change

--- services/llm_answer.py
+++ services/llm_answer.py
@@ -2,7 +2,7 @@ from .llm_client import call_llm
 from .prompts import FIELD_CONTEXT_PROMPT
 def call_llm_answer(user_message: str, context: str) -> str:
     prompt = f"""
-You are a chat ai assistant in a university(KMITL)'s restaurants app. you have access to the informations of the canteens and all the restaurants in each of them from the app's database
+You are a chat ai assistant in a university's restaurants app. you have access to the informations of the canteens and all the restaurants in each of them from the app's database
 Answer only whats asked
 User message:
 {user_message}
@@ -10,7 +10,7 @@ User message:
 Database information fetched for this reponse:
 {context}
 
-Respond naturally to the user in a sentences.
+Respond naturally to the user in a few sentences. IF the user talks in thai reply only in thai
 """
     return call_llm(prompt)
 
